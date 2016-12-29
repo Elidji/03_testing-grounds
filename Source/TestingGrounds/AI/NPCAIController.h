fcfe9682cb5a4317a77e494e5073501ff009a43d
kensai01 <br9975@wayne.edu>
2016-12-29 02:24:42 -0500

    Added a third person character to the player controller so the enemies see more than hands/gun; also works with death animation of player. Added a dynamically created level square tile blueprint that will spawn 10 levels as part of an Infinite Terrain Game Mode. Added a gating mechanism that allowed player to pass through but not backtrack. Separated out fire component so that two guns can each call the firing component (given that now the player character has a TP mesh /w gun as well as a FP mesh /w gun). Added an additive animation of firing-ironsights that is utilized in addition to the regular animation when the character is shooting. Added a death animation for enemy player. Added damage handling to enemy, hits are implemented on the projectile itself. Refactored the Animation Blueprint. Added notes to NPCAIController for the next time I decide to work on it.

--- Source/TestingGrounds/AI/NPCAIController.h
+++ Source/TestingGrounds/AI/NPCAIController.h
@@ -7,7 +7,27 @@
 #include "NPCAIController.generated.h"
 
 /**
- * 
+ * IN PROGRESS, REALLY MESSY AND NOT WORKING YET 
+
+	Implementation Notes before I forget later:
+	My instinct tells me that the reason this isn't working is because I have the 
+	behavior tree variable inside the AIController rather than the sentry pawn itself. 
+
+	Also possibly the BehaviorComp, need to investigate how BattleTanks implementation is set up.
+
+	To do this I will need to create a NPC character base class that derives from
+	ACharacter and then reparent the blueprint FirstPersonCharacter to this new derived class, 
+	call it ANPCCharacter or ASentryCharacter. ASentryCharacter will be a base class, 
+	except for the BehaviorTree components that I need inorder to run the assigned behavior tree. 
+
+	The rest of the implementation comes directly from blueprint I believe (this is why we reparent the current 
+	FirstPersonCharacter blueprint to the new class).
+
+	This way I will be able to assign the SentryBehavior behavior tree to the sentry pawn in blue print
+	and have it referenced direclty in C++ by the AIController. 
+
+	Besides this, I then need to rework the OnTargetPerceptionUpdated so that it casts to the pawn and gets the behavior 
+	tree to run and blackboard asset associated with this behavior tree directly from the Sentry character(NPC). 
  */
 
 UCLASS()
